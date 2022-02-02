a54d0c47ca4718314067caa7526afb51b57f7a2f
Simon <simon.speich@gmail.com>
2022-02-02 13:49:33 +0100

    - add permutating axes (numpy.tranpose() - exclude dimensions of size one only if dimensions with a lower index are also of size one - handle case where dimension is of size one and does not have an index property

--- src/Reader.php
+++ src/Reader.php
@@ -38,9 +38,13 @@ class Reader
      */
     #[Pure] public function getCategoryId(string $dimId, int $categIdx) {
         $dim = $this->data->dimension->{$dimId};
-        $index = $dim->category->index;
+        if (property_exists($dim->category, 'index')) {
+            $index = $dim->category->index;
 
-        return is_array($index) ? $index[$categIdx] : $this->categoryIdFromObject($index, $categIdx);
+            return is_array($index) ? $index[$categIdx] : $this->categoryIdFromObject($index, $categIdx);
+        }
+
+        return null;
     }
 
     /**
@@ -55,16 +59,39 @@ class Reader
 
     /**
      * Return list with the sizes of the dimensions.
-     * @param bool $excludeSizeOne do not return dimensions with size one
+     * Dimensions of size 1 are excluded by default, if each dimension with a lower index is also of size one, e.g.:
+     * [1,1,3,2,5] --> [3,2,5], but
+     * [1,1,3,2,1] --> [3,2,1]
+     * @param bool $excludeSizeOne do not return dimensions of size from the beginning
      * @return array list of sizes
      */
     public function getDimensionSizes(bool $excludeSizeOne = true): array
     {
-        $size = $excludeSizeOne ? 1 : 0;
-
-        return array_filter($this->data->size, static function ($value) use ($size) {
+        $size = 0;
+        $dimensions = $this->data->size;
+        return array_filter($dimensions, static function ($value, $idx) use ($excludeSizeOne, $dimensions, $size) {
+            if ($excludeSizeOne && self::continuous($dimensions, $idx)) {
+                $size = 1;
+            }
             return $value > $size;
-        });
+        }, ARRAY_FILTER_USE_BOTH);
+    }
+
+    /**
+     * Check if the $dimension at $idx is of size 1 and all dimensions with a lower index also.
+     * @param array $dimensions
+     * @param $idx
+     * @return bool
+     */
+    private static function continuous(array $dimensions, $idx): bool
+    {
+        for (; $idx > -1; $idx--) {
+            if ($dimensions[$idx] !== 1) {
+                return false;
+            }
+        }
+
+        return $idx === -1;
     }
 
     /**
@@ -86,9 +113,7 @@ class Reader
     public function getCategoryLabel(string $dimId, ?string $labelId = null): string
     {
         $dim = $this->data->dimension->{$dimId};
-        $index = $dim->category->index;
-        if ($index) {
-
+        if (property_exists($dim->category, 'index')) {
             $label = $dim->category->label->{$labelId} ?? $labelId;
         } else {  // e.g. constant dimension with a single category and no index, label is required
             $keys = array_keys((array)$dim->category->label);
@@ -109,5 +134,19 @@ class Reader
         $arr = array_keys((array)$obj);
 
         return array_search($labelIdx, $arr, true);
+    }
+
+    /**
+     * Permute the axes of the value array.
+     * @see https://numpy.org/doc/stable/reference/generated/numpy.transpose.html
+     * @param array $axes contains the permutations of [0,1,..,N-1] where N is the number of axes of the value array
+     * @return void
+     */
+    public function transpose(array $axes): void
+    {
+        $jsonstat = $this->data;
+        $jsonstat->value = UtilArray::transpose($jsonstat->value, $jsonstat->size, $axes);
+        $jsonstat->id = UtilArray::swap($jsonstat->id, $axes);
+        $jsonstat->size = UtilArray::swap($jsonstat->size, $axes);
     }
 }
