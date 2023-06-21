f4d6b723d0de78d915aa80d8588801ff397dc4e1
Simon Speich <info@speich.net>
2023-06-21 16:25:26 +0200

    clarify phpdoc

--- tests/phpunit/ReaderTest.php
+++ tests/phpunit/ReaderTest.php
@@ -2,6 +2,7 @@
 
 namespace jsonstatPhpViz\tests\phpunit;
 
+use DOMException;
 use JsonException;
 use jsonstatPhpViz\src\Reader;
 use jsonstatPhpViz\src\RendererTable;
@@ -21,17 +22,22 @@ class ReaderTest extends TestCase
         $this->reader = $reader->create(__DIR__ . '/../resources/volume.json');
     }
 
+    /**
+     * @throws DOMException
+     */
     public function testTranspose(): void
     {
-        /*
-        $html = file_get_contents(__DIR__.'/../resources/volume-transposed.html');
-
+        $html = file_get_contents(__DIR__ . '/../resources/volume-transposed.html');
         $this->reader->transpose([0, 1, 2, 4, 3, 5]);
         $table = new RendererTable($this->reader, 2);
+        $table->excludeOneDim = true;
         self::assertSame($html, $table->render());
-*/
     }
 
+    /**
+     * Test, that
+     * @return void
+     */
     public function testGetCategoryLabel(): void
     {
         $label = $this->reader->getCategoryLabel('BHDKL', '3');
@@ -54,10 +60,29 @@ class ReaderTest extends TestCase
         self::assertSame([6, 3, 6, 2], $size);
     }
 
+    /**
+     * Test, that the category id is returned, whether the index property is an array or an object.
+     * @return void
+     * @throws JsonException
+     */
     public function testGetCategoryId(): void
     {
-        $id = $this->reader->getCategoryId('concept', 0);
-        self::assertSame('1', $id);
+        // test id from array
+        $id = $this->reader->getCategoryId('BHDKL', 5);
+        self::assertSame('999999', $id);
+
+        // test id from object
+        $obj = json_decode('{
+            "0": 0,
+            "1": 1,
+            "2": 2,
+            "3": 3,
+            "4": 4,
+            "999999": 5
+        }', false, 512, JSON_THROW_ON_ERROR);
+        $this->reader->data->dimension->{'BHDKL'}->category->index = $obj;
+        $id = $this->reader->getCategoryId('BHDKL', 5);
+        self::assertSame('999999', $id);
     }
 
     public function testGetDimensionId(): void
