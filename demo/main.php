1f3070151088e1e463de77ed8ea43785a10b1414
Simon Speich <simon.speich@wsl.ch>
2023-06-22 14:34:22 +0200

    update demo

--- demo/main.php
+++ demo/main.php
@@ -3,20 +3,20 @@
 use jsonstatPhpViz\src\Reader;
 use jsonstatPhpViz\src\RendererTable;
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 // Render a table with 4 dimensions of size `[3,2,4,2]` (shape). To dimensions are used to group the rows:
-$filename = 'integer.json';
+$filename = '../tests/resources/integer.json';
 $json = file_get_contents($filename);
 $jsonstat = json_decode($json, false, 512, JSON_THROW_ON_ERROR);
 $reader = new Reader($jsonstat);
 $table = new RendererTable($reader);
-$table->caption.= ', dimension A and B used as row dimensions';
+$table->caption .= ', dimension A and B are used as row dimensions.';
 $html = $table->render();
 
 // Render the table with 3 dimensions used for the row grouping instead of 2 (default):
 $table = new RendererTable($reader, 3);
-$table->caption = 'Integers transposed: dimension A is swapped with Dimension C';
+$table->caption .= ', dimension A, B and C are used as row dimensions.';
 $html2 = $table->render();
 
 // Transpose table by permutating dimension A with dimension D. Also hide label of dimension A:
@@ -29,41 +29,50 @@ $html3 = $table->render();
 
 // Real-world example with data from the Swiss NFI having a caption, column units and row totals as well as
 // two dimensions of size one excluded from rendering.
-$filename = 'volume.json';
+$filename = '../tests/resources/volume.json';
 $json = file_get_contents($filename);
 $jsonstat = json_decode($json, false, 512, JSON_THROW_ON_ERROR);
 $reader = new Reader($jsonstat);
+
 $table = new RendererTable($reader);
-$table->excludeOneDim = true;
 $table->noLabelLastDim = true;
 $html4 = $table->render();
 
+$axes = [0, 4, 2, 3, 1, 5];
+$reader->transpose($axes);
+$table = new RendererTable($reader, 3);
+$table->excludeOneDim = true;
+$table->caption = 'Integers transposed: the second dimension PRODREG is swapped with the fifth dimension,
+ the first dimension (of size one) is not rendered.';
+$html5 = $table->render();
+
+
 // JSON-stat testdata
-$filename = '../tests/oecd.json';
+$filename = '../tests/resources/canada.json';
 $json = file_get_contents($filename);
 $jsonstat = json_decode($json, false, 512, JSON_THROW_ON_ERROR);
 $reader = new Reader($jsonstat);
-$table = new RendererTable($reader, 2);
-$html5 = $table->render();
+$table = new RendererTable($reader, 3);
+$html6 = $table->render();
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>JSON-stat table renderer</title>
-    <link rel="stylesheet" href="main.css">
-    <link rel="stylesheet" href="table.css">
+    <link rel="stylesheet" href="main.min.css">
+    <link rel="stylesheet" href="table.min.css">
 </head>
 <body class="theme1">
 <h1>Demo of rendering JSON-stat as html tables</h1>
-<?php echo $html; ?>
-<p><br><br></p>
-<?php echo $html2; ?>
-<p><br><br></p>
-<?php echo $html3; ?>
-<p><br><br></p>
-<?php echo $html4; ?>
-<p><br><br></p>
-<?php echo $html5; ?>
+<?=
+$html .
+$html2 .
+$html3 .
+$html4 .
+$html5 .
+$html6
+?>
 </body>
 </html>
