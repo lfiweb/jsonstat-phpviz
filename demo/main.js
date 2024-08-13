81bc8ccf746a9a374695f6c4ab30e5c0b39f0b93
Simon Speich <simon.speich@wsl.ch>
2024-08-13 11:04:13 +0200

    example how to use css/js to hide columns

--- demo/main.js
+++ demo/main.js
@@ -1,32 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     /* find the first th element with scope=colgroup of the row which is a previous sibling of the last row */
-    const cssQuery = 'tr:has(+ tr:last-of-type) th:nth-child(1 of [scope=colgroup])';
-    let colspanLastDim;
+    const cssQuery = '.lastDimSize3 tr:has(+ tr:last-of-type) th:nth-child(1 of [scope=colgroup])',
+     colspanLastDim = document.querySelector(cssQuery).colSpan;
 
-    document.querySelectorAll('.jst-viz')
-        .forEach(table => {
-            table.classList.add('ci-hidden');
+    // reduce the colspans of the header cells
+    document.querySelectorAll('.lastDimSize3 thead tr th[colspan]')
+        .forEach(th => {
+            th.colSpan -= th.colSpan / colspanLastDim;
         });
 
-    // 95%
-    colspanLastDim = document.querySelector(cssQuery).colSpan;
-    document.querySelectorAll('thead tr')
-        .forEach((tr, rowIdx) => {
-            tr.querySelectorAll('th[colspan]')
-                .forEach(th => {
-                    th.colSpan -= th.colSpan / colspanLastDim;  // * 2 for both confidence intervals
-                });
-
-        });
-
-    //
-    colspanLastDim = document.querySelector(cssQuery).colSpan;
-    document.querySelectorAll('table.lastDimSize3 thead tr')
-        .forEach((tr, rowIdx) => {
-            tr.querySelectorAll('th[colspan]')
-                .forEach(th => {
-                    th.colSpan -= th.colSpan / colspanLastDim;  // * 2 for both confidence intervals
-                });
-
-        });
 });
